ac2c7ba42aabba20907cac00433e15226319e446
Dale Morris <dalemorris2021@gmail.com>
2025-02-20 15:55:43 -0500

    Convert file data to workable data and back

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 )
@@ -20,5 +21,28 @@ func Run(config *Config) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(data))
+
+	raw := trim(data)
+	prettyPrint(raw)
+}
+
+func trim(data []byte) [][]byte {
+	raw := bytes.Split(data, []byte("\n"))
+	raw = raw[2 : len(raw)-1]
+
+	for i := range raw {
+		raw[i] = append([]byte("0"), raw[i][3:len(raw[i])-1]...)
+	}
+
+	return raw
+}
+
+func prettyPrint(raw [][]byte) {
+	const header string = `XX:                1               2               3
+XX:0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF`
+
+	fmt.Println(header)
+	for i, line := range raw {
+		fmt.Printf("%02X:%s\n", i, string(append(line[1:], []byte("0")...)))
+	}
 }
